4d33fbb2e7f095a56ee9f15717d8d54afe291bfa
Nawal2525 <karanstha197@gmail.com>
2023-03-25 20:04:06 +0545

    added bottom navigation

--- lib/view/screens/homescreen.dart
+++ lib/view/screens/homescreen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:tiktok_clone/const/color_const.dart';
+import 'package:tiktok_clone/view/widgets/custom_add_item.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,8 +10,61 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int pageId = 0;
+
+  List pageIndex = ['Home', 'Search', 'Upload Video', 'Messages', 'Profile'];
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Text(
+          pageIndex[pageId],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: pageId,
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: backGroundColor,
+        items: const [
+          BottomNavigationBarItem(
+            label: "Home",
+            icon: Icon(
+              Icons.home_rounded,
+              size: 25,
+            ),
+          ),
+          BottomNavigationBarItem(
+            label: "Search",
+            icon: Icon(
+              Icons.search_rounded,
+              size: 25,
+            ),
+          ),
+          BottomNavigationBarItem(
+            label: "",
+            icon: customAddIcon(),
+          ),
+          BottomNavigationBarItem(
+            label: "Messages",
+            icon: Icon(
+              Icons.message_rounded,
+              size: 25,
+            ),
+          ),
+          BottomNavigationBarItem(
+            label: "Profile",
+            icon: Icon(
+              Icons.person_rounded,
+              size: 25,
+            ),
+          ),
+        ],
+        onTap: (index) {
+          setState(() {
+            pageId = index;
+          });
+        },
+      ),
+    );
   }
 }
